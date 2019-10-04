2f774e20e721f54a40b9eb20b8fb4a91b805be75
Andres Guillermo Velasquez <andres.velasquez@co-it004961.globant.com>
2019-10-04 15:56:47 -0500

    Solved https://www.hackerrank.com/challenges/fibonacci-modified/problem

---
+++ Fibonacci_Modified/Fibonacci_Modified.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the fibonacciModified function below.
+def fibonacciModified(t1, t2, n):
+    modified_f = [t1, t2]
+
+    for t in range(2, n):
+        modified_f.append(modified_f[t - 2] + modified_f[t - 1]**2)
+    print(modified_f)
+    return modified_f[n - 1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t1T2n = input().split()
+
+    t1 = int(t1T2n[0])
+
+    t2 = int(t1T2n[1])
+
+    n = int(t1T2n[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close(
