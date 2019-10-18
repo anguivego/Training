c4b28c2e8e389216b8cde651c767735b10439ae0
Andres Guillermo Velasquez <anguivego@gmail.com:wq>
2019-10-18 16:42:01 -0500

    WP

--- Equal/equal.py
+++ Equal/equal.py
@@ -7,39 +7,47 @@ import re
 import sys
 
 # Complete the equal function below.
-operations = []
-def are_equal(A):
-    init_a =A[0]
-    for i in A:
-        if i != init_a:
-            return 0
-    return 1
-
-def f(i, j, nop):
+memory = {}
+def f(i, j, nop, rank):
+    global memory
     if i == j:
         return 0
 
     tmp_min = [math.inf]
-    distributed_c = 0
-    C = [1, 2, 5]
+
+    local_min = math.inf 
+    operatios = []
     for nc in [1, 2, 5]:
         if nop > 0 :
             if i < j :
-                tmp_min.append(f(i + nc, j, nop - 1) + 1)
+               # print(rank, i, ".", j,"->", rank + 1, i + nc, ".", j, "[ label=", nc, " ]", ";", sep="")
+                str_index = str(i + nc) + "," + str(j)
+                if memory.get(""):
+                    tmp_m = memory[str_index] 
+                else:
+                    tmp_m = f(i + nc, j, nop - 1, rank + 1) + 1
+                    memory[str_index] =  tmp_m
+
+                tmp_min.append(tmp_m)
             else:
-                tmp_min.append(f(i , j + nc, nop - 1) + 1)
-        else:
-            return 0
-    op_index = tmp_min.index(min(tmp_min))
-    operations.append(C[op_index - 1])
+               # print(rank, i,".",j,"->", rank + 1, i ,".", j + nc,"[ label=", nc, " ]",";", sep="")
+                str_index = str(i ) + "," + str(j + nc)
+                if memory.get(""):
+                    tmp_m = memory[str_index] 
+                else:
+                    tmp_m = f(i , j + nc, nop - 1, rank + 1) + 1
+                    memory[str_index] =  tmp_m
     return min(tmp_min)
 
     
 
 def equal(arr):
-    i = 3
-    j = 8 
-    print(f(i, j, j - i), operations)
+    n_op = 0
+    for i in range(1, len(arr)):
+        n_op = n_op + f(arr[i - 1], arr[i],abs(arr[i - 1] -  arr[i]),0 )
+        if i + 1 < len(arr):
+            arr[i + 1] = arr[i + 1] + abs(arr[i - 1] -  arr[i])
+    print(memory)
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
